b9fc8acd329b11b0f5e84dcc3ecb63727196eb61
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-02-04 19:28:31 -0300

    Feature: Final do Modulo_01

--- index.js
+++ index.js
@@ -3,8 +3,75 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-    return res.json({ message: 'hello world' });
+server.use(express.json());
+
+const users = ['Sindba', 'Diego', 'Maria'];
+
+server.use((req, res, next) => {
+    console.time('Request');
+    
+    console.log(`Method: ${req.method};\nURL: ${req.url};`);
+
+    next();
+
+    console.timeEnd('Request');
+
+
+});
+
+function checkUserExists(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({ error: "User name is required" });
+    };
+
+    return next();
+};
+
+function checkUserInArray(req, res, next) {
+    const user = users[req.params.index];
+
+    if(!users[req.params.index]){
+        return res.status(400).json({ error: 'User does not exists'});
+    };
+
+    req.user = user;
+
+    return next();
+};
+
+server.get('/users', (req, res) => {
+    return res.json(users);
+});
+
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    const { index } = req.params;
+    
+    return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+});
+
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
+    const { name } = req.body;
+    const { index } = req.params;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+server.delete('/users/:index', checkUserInArray, (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+    
+    return res.send("Sucesso");
 });
 
 server.listen(3000);
